76b8ecdf87b7f3e2d1fbb0e13b8930b99b9e36fc
João Pedro <115329370+jpbdalmagro@users.noreply.github.com>
2024-08-14 11:07:50 -0300

    att de codigo

--- primos2.py
+++ primos2.py
@@ -1,6 +1,17 @@
-x = int(input("Digite um número: "))
-y = int(input("Digite outro número: "))
+erro = True
 
+while erro:
+    x = int(input("Digite um número: "))
+    y = int(input("Digite outro número: "))
+    erro = False
+    if x <= 0 | y <= 0:
+        print("Aceito somente números positivos!")
+        erro = True
+    else:
+        if y > x:
+            primo("O inicio não pode ser maior que o fim!")
+            erro = True
+            
 print(f"Os números primos entre {x} e {y} são:")
 
 while x <= y:
