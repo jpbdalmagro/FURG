e8ea7ae2b5f0cb052b7290842033fe04f14a4a67
João Pedro <115329370+jpbdalmagro@users.noreply.github.com>
2024-08-16 10:19:37 -0300

    .

--- Lista_EX/ex006.py
+++ Lista_EX/ex006.py
@@ -1,3 +1,5 @@
+#Faça um programa em python que leia 3 números e os mostre em ordem crescente.
+
 n1 = int(input("Digite um número: "))
 n2 = int(input("Digite outro número: "))
 n3 = int(input("Digite um último número: "))
