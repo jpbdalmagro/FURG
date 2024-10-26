8159691015d16d58eb384c598cb7d03ed478e78c
João Pedro <115329370+jpbdalmagro@users.noreply.github.com>
2024-10-25 23:14:45 -0300

    Update and rename contador3.py to Lista_EX/ex12.py

---
+++ Lista_EX/ex12.py
@@ -0,0 +1,8 @@
+#Escreva um programa que mostre os números pares de 1 a 200 
+
+cont = 2
+
+while cont <= 200:
+    print(cont)
+    cont += 2
+    
--- contador3.py
+++
@@ -1,4 +0,0 @@
-cont = 2
-while cont <= 200:
-    print(cont)
-    cont += 2
