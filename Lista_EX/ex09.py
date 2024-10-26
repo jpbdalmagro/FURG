debc71b31bffa2aa3fd7586cc372b5329f9df30c
João Pedro <115329370+jpbdalmagro@users.noreply.github.com>
2024-10-25 23:11:01 -0300

    Update and rename imposto.py to Lista_EX/ex09.py

--- Lista_EX/ex09.py
+++ Lista_EX/ex09.py
@@ -1,3 +1,6 @@
+# Crie um programa em Python que leia o rendimento mensal do usuário,
+# qual o modelo de imposto (sem correção/com correção das perdas no governo Bolsonaro) e retorne o quanto ele deve pagar de imposto.
+
 salario = float(input("Digite seu salário: "))
 
 def imposto_sem_correcao(salario):
